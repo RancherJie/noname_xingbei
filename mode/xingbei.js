59193ac25f1013fec5455529a57579363ecbfabe
NongJie <245722665@qq.com>
2024-04-28 19:51:02 +0800

    修复移除指示物超过持有时的bug

--- mode/xingbei.js
+++ mode/xingbei.js
@@ -3586,7 +3586,13 @@ game.import('mode',function(lib,game,ui,get,ai,_status){
 				},
 				removeZhiShiWu:function(zhiShuWu,num){
 					if(typeof num!='number'||!num) num=1;
-					this.removeMark(zhiShuWu,num);
+					var current=this.countMark(zhiShuWu);
+					if(current+num<0){
+						num=-current;
+					}
+					if(num<0){
+						this.removeMark(zhiShuWu,-num);
+					}
 				},
 				addZhanJi:function(color,num){
 					if(typeof num!='number'||!num) num=1;
