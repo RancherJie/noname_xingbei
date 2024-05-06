5b0c89f5c0a5075600d1f43481c9c44ff8afec2a
NongJie <245722665@qq.com>
2024-05-06 15:55:24 +0800

    更新v0.2.10

--- game/update.js
+++ game/update.js
@@ -1,10 +1,11 @@
 window.noname_update={
-	version:'0.2.9',
-	update:'0.2.9',
+	version:'0.2.10',
+	update:'0.2.10',
 	changeLog:[
 		
 	],
 	files:[
-		
+		'image/card/cardtempname_bg.png',
+		'image/card/cardtempname_bg.png',
 	]
 };
