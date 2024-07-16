8f05e186759091adf8c74253fee9ccdc5b2840b6
NongJie <245722665@qq.com>
2024-07-16 19:06:59 +0800

    修复神圣追击执行特殊行动时可以发动

--- character/shiZhouNian.js
+++ character/shiZhouNian.js
@@ -1519,7 +1519,7 @@ game.import('character',function(lib,game,ui,get,ai,_status){
             shenShengZhuiJi:{
                 trigger:{player:['useCardAfter','useSkillAfter']},
                 filter:function(event,player){
-                    return player.zhiLiao>0&&get.is.xiDong(event);
+                    return player.zhiLiao>0&&(get.is.faShuXingDong(event)||get.is.gongJiXingDong(event));
                 },
                 content:function(){
                     player.changeZhiLiao(-1);
