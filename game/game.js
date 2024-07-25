1dc1cad7585fef70dd4ee7217f08390299567c29
NongJie <245722665@qq.com>
2024-07-25 11:39:44 +0800

    删除无用布局

--- game/game.js
+++ game/game.js
@@ -1299,8 +1299,8 @@
 							//default:'旧版',
 							newlayout:'对称',
 							mobile:'默认',
-							long:'宽屏',
-							long2:'手杀',
+							//long:'宽屏',
+							//long2:'手杀',
 							nova:'新版'
 						},
 						visualMenu:function(node,link){
