91eaeafc8c1ac06a1f4b15b9251bd75ec89604cc
NongJie <245722665@qq.com>
2024-08-04 15:54:51 +0800

    修复BP模式，改变可选角色数报错

--- mode/xingbei.js
+++ mode/xingbei.js
@@ -1820,7 +1820,7 @@ game.import('mode',function(lib,game,ui,get,ai,_status){
 					//console.log('chooseCharacterOLCM02');
 					//var ref=game.players[0];
 					event.number=lib.configOL.number;
-					event.choose_number=lib.configOL.BPchoose_number;
+					event.choose_number=parseInt(lib.configOL.BPchoose_number);
 
 					var team_sequence=lib.configOL.team_sequence;
 
@@ -2246,7 +2246,7 @@ game.import('mode',function(lib,game,ui,get,ai,_status){
 					//console.log('chooseCharacterOLCM02');
 					//var ref=game.players[0];
 					event.number=lib.configOL.number;
-					event.choose_number=event.choose_number=lib.configOL.BPchoose_number;;
+					event.choose_number=parseInt(lib.configOL.BPchoose_number);
 
 					var team_sequence=lib.configOL.team_sequence;
 
