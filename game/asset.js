d10ce715b528ec7116e5abb4c2785a87d897ea7e
NongJie <245722665@qq.com>
2024-08-07 15:50:19 +0800

    更新素材版本

--- game/asset.js
+++ game/asset.js
@@ -1,5 +1,5 @@
 window.noname_asset_list=[
-	'v0.4.1',
+	'v0.6.3',
 	/*audio start*/
 	'audio/background/Made_in_Abyss.mp3',
 	/*cardaudio start*/
@@ -117,6 +117,7 @@ window.noname_asset_list=[
 	'image/card/tiaoXin.png',
 	'image/card/lingHunLianJie.png',
 	'image/card/tongShengGongSi.png',
+	'image/card/yiDuanCaiJueSuo.png',
 	/*card image end*/
 	
 	/*character image begin*/
