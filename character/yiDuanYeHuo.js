6381d9b726087e0cc67b7a595f8303c5b6ead672
NongJie <245722665@qq.com>
2024-08-16 07:59:33 +0800

    修复恩典神授可提炼超过2个星石

--- character/yiDuanYeHuo.js
+++ character/yiDuanYeHuo.js
@@ -483,6 +483,7 @@ game.import('character',function(lib,game,ui,get,ai,_status){
                     }
                     'step 2'
                     var num=target.getNengLiangLimit()-target.countNengLiangAll();
+                    num=Math.min(num,2);
                     if(player.side==true){
                         var list=game.hongZhanJi;
                     }else if(player.side==false){
