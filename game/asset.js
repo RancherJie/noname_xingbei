bbc9f12104dd0fdb675240842a05db2a39c88d94
NongJie <245722665@qq.com>
2024-08-31 12:06:50 +0800

    更新素材版本

--- game/asset.js
+++ game/asset.js
@@ -1,5 +1,5 @@
 window.noname_asset_list=[
-	'v0.6.4',
+	'v0.7.3',
 	/*audio start*/
 	'audio/background/Made_in_Abyss.mp3',
 	/*cardaudio start*/
@@ -122,6 +122,7 @@ window.noname_asset_list=[
 	'image/card/yingYue.png',
 	'image/card/shiRi.png',
 	'image/card/jueJie.png',
+	'image/card/fengXue.png',
 	/*card image end*/
 	
 	/*character image begin*/
