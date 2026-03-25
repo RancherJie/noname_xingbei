37840202d22a5690570289980e01ccd9ec2fdca7
NongJie <245722665@qq.com>
2026-03-25 13:12:09 +0800

    将星杯传说模式名改为单机

--- game/package.js
+++ game/package.js
@@ -17,7 +17,7 @@ window.noname_package = {
 
 	},
 	mode: {
-		xingBei: "星杯传说",
+		xingBei: "单机",
 		boss: "BOSS",
 		connect: "联机",
 		offlineChoose: "线下选角",
--- noname/library/index.js
+++ noname/library/index.js
@@ -4777,7 +4777,7 @@ export class Library {
 	};
 	mode = {
 		xingBei:{
-			name:'星杯传说',
+			name:'单机',
 			connect:{
 				update:function(config,map){		
 					if(config.connect_phaseswap){
