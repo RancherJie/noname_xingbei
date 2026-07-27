4b1e1fb067f0048d8e57fc18b6ce6a31ad539fa0
NongJie <245722665@qq.com>
2026-07-27 20:28:49 +0800

    优化damageEffect函数

--- noname/get/index.js
+++ noname/get/index.js
@@ -5559,9 +5559,9 @@ export class Get extends GetCompatible {
 	damageEffect(target,num){
 		if(!target) return 0;
 		if(!num) num=2;
-		if(target.hasSkillTag('noShiQiXiaJiang')) return 0.1;
+		if(target.hasSkillTag('noShiQiXiaJiang')) return 0;
 		var chaZhi=target.getHandcardLimit()-target.countCards('h');
-		if(target.hasSkillTag('oneDamage')&&num==1) return 0;
+		if(target.hasSkillTag('oneDamage')&&num==1) return 0.5;
 		if(chaZhi<num) return -(num-chaZhi)-1;
 		else if(chaZhi-3<num) return -1;
 		else return -0.5;
